ad50c2cd757ee84cbe39594dea8eb14da1eed7b8
Pedro Henrique <karpatech2025@gmail.com>
2025-07-10 19:06:32 -0300

    conta-bancaria

--- menu.ts
+++ menu.ts
@@ -1,105 +1,237 @@
 import readlinesync = require("readline-sync");
-import { colors } from "./src/util/colorS";
+import { Colors } from "./src/util/colorS";
 import { Conta } from "./src/util/model/conta";
+import { ContaCorrente } from "./src/util/model/ContaCorrente";
+import { ContaPoupanca } from "./src/util/model/ContaPoupanca";
 
 export function main() {
+  let opcao: number;
 
-    let opcao: number;
+  // Objeto da Classe Conta (Teste)
+  const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+  conta.visualizar();
+  conta.sacar(10500);
+  conta.visualizar();
+  conta.depositar(5000);
+  conta.visualizar();
 
-        // Objeto da Classe Conta (Teste)
-    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
-    conta.visualizar();
-    conta.sacar(10500);
-    conta.visualizar();
-    conta.depositar(5000);
-    conta.visualizar();
+  const contacorrente: ContaCorrente = new ContaCorrente(
+    2,
+    123,
+    1,
+    "Mariana",
+    15000,
+    1000
+  );
+  contacorrente.visualizar();
+  contacorrente.sacar(2000);
+  contacorrente.visualizar();
+  contacorrente.depositar(1000);
+  contacorrente.visualizar();
 
+  const contapoupanca: ContaPoupanca = new ContaPoupanca(
+    3,
+    123,
+    2,
+    "Victor",
+    1000,
+    10
+  );
+  contapoupanca.visualizar();
+  contapoupanca.sacar(200);
+  contapoupanca.visualizar();
+  contapoupanca.depositar(1000);
+  contapoupanca.visualizar();
 
-    while (true) {
-        console.log(colors.bg.black, colors.fg.yellow, "*****************************************************");
-        console.log("                                                     ");
-        console.log("                BANCO DA PERIFERIA              ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ");
-        console.log("            1 - Criar Conta                          ");
-        console.log("            2 - Listar todas as Contas               ");
-        console.log("            3 - Buscar Conta por Número              ");
-        console.log("            4 - Atualizar Dados da Conta             ");
-        console.log("            5 - Apagar Conta                         ");
-        console.log("            6 - Sacar                                ");
-        console.log("            7 - Depositar                            ");
-        console.log("            8 - Transferir valores entre Contas      ");
-        console.log("            9 - Sair                                 ");
-        console.log("                                                     ");
-        console.log("*****************************************************");
-        console.log("                                                     ",colors.reset);
+  while (true) {
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.yellow,
+      "                                                    ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "                                                     ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "                  BANCO PERIFERIA                   ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "                                                     ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "|||||||||||||||||||||||||||||||||||||||||||||||||||||",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "                                                     ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            1 - Criar Conta                         ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            2 - Listar todas as Contas              ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            3 - Acessa Conta                        ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            4 - Atualizar Dados da Conta            ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            5 - Apagar Conta                        ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            6 - Sacar                               ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            7 - Depositar                           ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            8 - Transferir valores entre Contas     ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      Colors.fg.bluestrong,
+      "            9 - Sair                                ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "                                                     ",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "|||||||||||||||||||||||||||||||||||||||||||||||||||||",
+      Colors.reset
+    );
+    console.log(
+      Colors.bg.whitebright,
+      "                                                     ",
+      Colors.reset
+    );
 
-        console.log(colors.fg.greenstrong,"Escolha uma opção: ", colors.reset);
-        opcao = readlinesync.questionInt("");
+    console.log(Colors.fg.greenstrong, "Escolha uma opção: ", Colors.reset);
+    opcao = readlinesync.questionInt("");
 
-        if (opcao == 9) {
-            console.log(colors.fg.greenstrong,"\nBanco Da Periferia - De Nós Pra Nós!");
-            sobre();
-            console.log(colors.reset, "")
-            process.exit(0);
-        }
-
-        switch (opcao) {
-            case 1:
-                console.log(colors.fg.bluestrong,"\n\nCriar Conta\n\n", colors.reset);
-
-                break;
-            case 2:
-                console.log(colors.fg.bluestrong,"\n\nListar todas as Contas\n\n", colors.reset);
-
-                break;
-            case 3:
-                console.log(colors.fg.bluestrong,"\n\nConsultar dados da Conta - por número\n\n", colors.reset);
-
-                break;
-            case 4:
-                console.log(colors.fg.bluestrong,"\n\nAtualizar dados da Conta\n\n", colors.reset);
-
-                break;
-            case 5:
-                console.log(colors.fg.bluestrong,"\n\nApagar uma Conta\n\n", colors.reset);
-
-                break;
-            case 6:
-                console.log(colors.fg.bluestrong,"\n\nSaque\n\n", colors.reset);
-
-                break;
-            case 7:
-                console.log(colors.fg.bluestrong,"\n\nDepósito\n\n", colors.reset);
-
-                break;
-            case 8:
-                console.log(colors.fg.bluestrong,"\n\nTransferência entre Contas\n\n", colors.reset);
-
-                break;
-            default:
-                console.log(colors.fg.bluestrong,"\nOpção Inválida!\n", colors.reset);
-
-                break;
-        }
+    if (opcao == 9) {
+      console.log(Colors.fg.greenstrong, "\nBanco Periferia - De Nós Pra Nós!");
+      sobre();
+      console.log(Colors.reset, "");
+      process.exit(0);
     }
 
+    switch (opcao) {
+      case 1:
+        console.log(Colors.fg.bluestrong, "\n\nCriar Conta\n\n", Colors.reset);
+
+        break;
+      case 2:
+        console.log(
+          Colors.fg.bluestrong,
+          "\n\nListar todas as Contas\n\n",
+          Colors.reset
+        );
+
+        break;
+      case 3:
+        console.log(
+          Colors.fg.bluestrong,
+          "\n\nConsultar dados da Conta - por número\n\n",
+          Colors.reset
+        );
+
+        break;
+      case 4:
+        console.log(
+          Colors.fg.bluestrong,
+          "\n\nAtualizar dados da Conta\n\n",
+          Colors.reset
+        );
+
+        break;
+      case 5:
+        console.log(
+          Colors.fg.bluestrong,
+          "\n\nApagar uma Conta\n\n",
+          Colors.reset
+        );
+
+        break;
+      case 6:
+        console.log(Colors.fg.bluestrong, "\n\nSaque\n\n", Colors.reset);
+
+        break;
+      case 7:
+        console.log(Colors.fg.bluestrong, "\n\nDepósito\n\n", Colors.reset);
+
+        break;
+      case 8:
+        console.log(
+          Colors.fg.bluestrong,
+          "\n\nTransferência entre Contas\n\n",
+          Colors.reset
+        );
+
+        break;
+      default:
+        console.log(Colors.fg.bluestrong, "\nOpção Inválida!\n", Colors.reset);
+
+        break;
+    }
+  }
 }
 
 /* Função com os dados da pessoa desenvolvedora */
 
 export function sobre(): void {
-    console.log("\n*****************************************************");
-    console.log("Projeto Desenvolvido por: Generation Brasil - generation@generation.org ");
-    console.log("Karpa Tech - karpatech2025@gmail.com");
-    console.log("https://github.com/KarpaTech");
-    console.log("*****************************************************");
+  console.log("\n*****************************************************");
+  console.log(
+    "Projeto Desenvolvido por: Generation Brasil - generation@generation.org "
+  );
+  console.log("Karpa Tech - karpatech2025@gmail.com");
+  console.log("https://github.com/KarpaTech");
+  console.log("*****************************************************");
 }
 
 function keyPress(): void {
-    console.log(colors.reset, "");
-    console.log("\nEntrar.");
-    readlinesync.prompt();
+  console.log(Colors.reset, "");
+  console.log("\nEntrar.");
+  readlinesync.prompt();
 }
 main();
